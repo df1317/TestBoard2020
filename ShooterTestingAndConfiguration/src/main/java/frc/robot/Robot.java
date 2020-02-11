2883483d9a94ac9c60a872dc1b7be78eda078d06
JonathanRYoung <jryoung4@icloud.com>
2020-02-10 21:58:17 -0500

    idk wha happun'

--- ShooterTestingAndConfiguration/src/main/java/frc/robot/Robot.java
+++ ShooterTestingAndConfiguration/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,9 @@ import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.ColorMatch;
 import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
@@ -15,11 +18,13 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class Robot extends TimedRobot {
+
 
 //_______________Declarations_______________
 
@@ -63,7 +68,7 @@ public class Robot extends TimedRobot {
 	boolean winchReverse;
 	int POVhook;
 	boolean stationCollection;
-	boolean colorRotations;
+	boolean colorRotate;
 	boolean colorEndgame;
 	boolean gearShift;
 	boolean resetButton1;
@@ -78,16 +83,20 @@ public class Robot extends TimedRobot {
 	boolean endRotation;
 	boolean allRotationsDone = false;
 	String gameData;
-	Timer timeTest = new Timer();
+	Timer TestDelta = new Timer();
 	double X;
 	double Y;
 	double Z;
+	double distance;
 
-	public void autonomousInit() {
-		//valid auto init code here
-	}
+	BuiltInAccelerometer accel2;
+
+	Accelerometer accel;
+	double AccelerometerX;
+
 	// This function is called once at the beginning during operator control
 	public void robotInit() {
+		TestDelta.start();
 
 		try {
             ahrs = new AHRS(SPI.Port.kMXP); 
@@ -100,17 +109,29 @@ public class Robot extends TimedRobot {
 		m_colorMatcher.addColorMatch(kYellowTarget);
 	}
 
-	public void autonomousPeriodic() {
-		
-	}
-
 	// This function is called periodically during operator control
 	public void robotPeriodic() {
+		double currentDeltaTIme = TestDelta.get();
+		SmartDashboard.putNumber("deltaTime", currentDeltaTIme);
+		double distanceSinceReset = ahrs.getVelocityX() * currentDeltaTIme;
+		distance = distanceSinceReset + distance;
+		TestDelta.reset();
+		SmartDashboard.putNumber("distance", distance);
+		SmartDashboard.putNumber("meters/sec", ahrs.getVelocityX());
+
+		//accel2.getX();
 
 		//gyro
-		X = ahrs.getRoll();
+		/*X = ahrs.getRoll();
+		SmartDashboard.putNumber("Roll", X);
 		Y = ahrs.getPitch();
+		SmartDashboard.putNumber("Pitch", Y);
 		Z = ahrs.getYaw();
+		SmartDashboard.putNumber("Yaw", Z);*/
+
+		
+		//trying out the displacement thingy
+		SmartDashboard.putBoolean("isCalibrating", ahrs.isCalibrating());
 
 		//get joystick values and buttons and such
 		RightVal = joyR.getY();
@@ -122,11 +143,12 @@ public class Robot extends TimedRobot {
 		winchReverse = joyE.getRawButton(12);
 		POVhook = joyE.getPOV();
 		stationCollection = joyE.getRawButton(3);
-		colorRotations = joyE.getRawButton(4);
+		colorRotate = joyE.getRawButton(4);
 		colorEndgame = joyE.getRawButton(6);
 		gearShift = joyR.getRawButton(1);
 		resetButton1 = joyE.getRawButton(7);
 		resetButton2 = joyE.getRawButton(8);
+		//reset button 2 for the various pistons of the robot
 		
 		//DriveTrain
 		FRMotor.set(RightVal);
@@ -168,6 +190,24 @@ public class Robot extends TimedRobot {
 			System.out.println("Ejecting balls from collector");
 		}
 	
+		//Resetting the color wheel values and whatnot
+		if (resetButton1) {
+			allRotationsDone = false;
+			halfRotation = 0;
+			endRotation = false;
+		}
+		//Resetting the pistons to default positions
+		if (resetButton2) {
+			CollectionDoor.set(DoubleSolenoid.Value.kForward);
+			SwifferPiston.set(DoubleSolenoid.Value.kReverse);
+		}
+
+		//turning the motors off if they are not in use
+		if (!eject && !ballShooter && !stationCollection && !groundCollection) {
+			BeltMotor.set(0);
+			SwifferMotor.set(0);
+		}
+	
 		//Classic endgame question of "what color do we need to get again???"
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
 		if(gameData.length() > 0) {
@@ -187,45 +227,45 @@ public class Robot extends TimedRobot {
 		}
 		
 		//Color Sensor functions
-		currentColor = colorSensor.getColor();
-		match = m_colorMatcher.matchClosestColor(currentColor);
-		if (match.color == kGreenTarget) {
-			colorString = "Green";
-			color = 2;
-			endRotation = true;
-		} else if (match.color == kYellowTarget) {
-			colorString = "Yellow";
-			color = 4;
-		} else if (match.color == kBlueTarget) {
-			colorString = "Blue";
-			color = 3;
-		} else if (match.color == kRedTarget) {
-			colorString = "Red";
-			color = 1;
-		} else {
-			colorString = "Unknown";
-			color = 0;
-			fieldColor = 0;
-		}
-		if(color != 0) fieldColor = (color+2)%4;
-		if (fieldColor == 1 && endRotation) {
-			halfRotation = halfRotation + 1;
-			endRotation = false;
+		if (colorRotate || colorEndgame) {
+			currentColor = colorSensor.getColor();
+			match = m_colorMatcher.matchClosestColor(currentColor);
+			if (match.color == kGreenTarget) {
+				colorString = "Green";
+				color = 2;
+				endRotation = true;
+			} else if (match.color == kYellowTarget) {
+				colorString = "Yellow";
+				color = 4;
+			} else if (match.color == kBlueTarget) {
+				colorString = "Blue";
+				color = 3;
+			} else if (match.color == kRedTarget) {
+				colorString = "Red";
+				color = 1;
+			} else {
+				colorString = "Unknown";
+				color = 0;
+				fieldColor = 0;
+			}
+			if(color != 0) fieldColor = (color+2)%4;
+			if (fieldColor == 1 && endRotation) {
+				halfRotation = halfRotation + 1;
+				endRotation = false;
+			} 
+			SmartDashboard.putNumber("Confidence", match.confidence);
+			SmartDashboard.putString("Detected Color", colorString);
 		}	
-    	SmartDashboard.putNumber("Confidence", match.confidence);
-		SmartDashboard.putString("Detected Color", colorString);		
+		
 
 		//Normal Color wheel functions
 		if (halfRotation == 7) {
 			allRotationsDone = true;
 		}
-		if (colorRotations && !allRotationsDone) {
+		
+		//Rotating the Color wheel 3 ish rotations
+		if (colorRotate && !allRotationsDone) {
 			ColorMotor.set(ControlMode.PercentOutput, ColorMotorVal);
-		}
-		if (resetButton1) {
-			allRotationsDone = false;
-			halfRotation = 0;
-			endRotation = false;
 		}
 
 		//endgame color wheel, Jonathan's design, Erin's execution, and Jacob's incredible clean-up skillz.
